54164d760c2415076bc9aff4a67d415eac5087de
bernardo1574 <bernardopiero12330@gmail.com>
2021-09-11 15:35:27 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+<<<<<<< HEAD
+import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/categories', categoriesRoutes);
+
+app.listen(3333, () => console.log('listening on port 3333'));
+=======
+>>>>>>> 811642b6356cec48ac1d95f7fa4c51eb5e4f7e3d
