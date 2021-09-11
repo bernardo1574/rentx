976123fccd66b54a4e01ac32e9f45e5b6dcc9bc6
bernardo1574 <bernardopiero12330@gmail.com>
2021-09-11 15:40:04 -0300

    update directory interfaces

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -2,7 +2,7 @@ import { Category } from '../model/Category';
 import {
   ICategoriesRepository,
   ICreateCategoryDTO,
-} from './ICategoriesRepository';
+} from '../interfaces/ICategoriesRepository';
 
 class CategoriesRepository implements ICategoriesRepository {
   private categories: Category[];
