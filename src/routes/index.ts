1aa79c442b02e0b9680b1eecf832cbc94ef62a83
Bernardo  Pieropan <bernardopiero12330@gmail.com>
2022-02-22 15:03:07 -0300

    route authentication

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import { accountRoutes } from './accounts.routes';
 import { categoriesRoutes } from './categories.routes';
 import { specificationRoutes } from './specifications.routes';
+import { authenticateRoutes } from './authenticate.routes';
 
 const router = Router();
 
 router.use('/categories', categoriesRoutes);
 router.use('/specifications', specificationRoutes);
 router.use('/accounts', accountRoutes);
+router.use(authenticateRoutes);
 
 export { router };
